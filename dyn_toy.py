872be756a0e2b26930adcfdfec6c6799c278d365
sboyer_dell <fedoboys@hotmail.com>
2024-02-29 10:53:57 -0500

    Made the system open instead of closed

--- dyn_toy.py
+++ dyn_toy.py
@@ -11,30 +11,28 @@ r13 = 1
 r23 = 1
 
 # initial concentrations 
-x1_0 = 10
+x1_0 = 5
 x2_0 = 0
 x3_0 = 0
 
 
 # initial reservoir values 
-R1_0 = 100
-R2_0 = 0
+R1 = 10
+R2 = 0
 
 # initial vector
-z0 = [x1_0, x2_0, x3_0, R1_0, R2_0]
+z0 = [x1_0, x2_0, x3_0]
 
 def toydt(t, z, k1, k2, r12, r13, r23):
     # z in this case will be a vector containing x1, x2, x3, and reservoir values 
-    x1, x2, x3, R1, R2 = z
+    x1, x2, x3 = z
     dx1dt = (-r12 - r13)*x1 + k1*(R1-x1) + r12*x2 + r13*x3
     dx2dt = (-r12 - r23)*x2 + k2*(R2-x2) + r12*x1 + r23*x3 
     dx3dt = r13*x1 + r23*x2 - (r13 + r23)*x3
 
 
-    dR1dt = -k1*(R1-x1)
-    dR2dt = -k2*(R2-x2)
 
-    return [dx1dt, dx2dt, dx3dt, dR1dt, dR2dt]
+    return [dx1dt, dx2dt, dx3dt]
 
 tf = 5
 
@@ -45,7 +43,7 @@ z = sol.sol(t)
 plt.plot(t, z.T)
 plt.xlabel('t')
 plt.ylabel('conc')
-plt.legend(['x1', 'x2', 'x3', 'R1', 'R2'], shadow=True)
+plt.legend(['x1', 'x2', 'x3'], shadow=True)
 plt.show()
 
 
---
+++ rxn_sim.py
@@ -0,0 +1,2 @@
+# First case 
+# we have x1, x2, and x2
