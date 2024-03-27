caa27e0289725139e8edf425299a04649ebab521
sboyer_dell <fedoboys@hotmail.com>
2024-03-27 13:12:54 -0400

    Added rxn_sim.py; script for flux based toy model

--- dyn_toy.py
+++ dyn_toy.py
@@ -4,21 +4,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # reaction constants 
-k1 = 10
-k2 = 10
-r12 = 5
+k1 = 1 
+k2 = 1 
+r12 = 1
 r13 = 1
 r23 = 1
 
 # initial concentrations 
-x1_0 = 5
-x2_0 = 0
-x3_0 = 0
+x1_0 = 0
+x2_0 = 0 
+x3_0 = 0 
 
 
 # initial reservoir values 
-R1 = 10
+R1 = 2 
 R2 = 0
+
+# chemical potentials
+muR1 = 10
+muR2 = 0
+
+np.random.seed(42)
+mu1, mu2, mu3 = np.random.uniform(muR2, muR1, 3)
+# mu1 = 7
+# mu2 = 1
+# mu3 = 9
+
+# thermo 
+dg13 = mu3 - mu1 
+dg12 = mu2 - mu1 
+dg23 = mu3 - mu2  # check if this sign is right 
+
 
 # initial vector
 z0 = [x1_0, x2_0, x3_0]
@@ -26,15 +42,18 @@ z0 = [x1_0, x2_0, x3_0]
 def toydt(t, z, k1, k2, r12, r13, r23):
     # z in this case will be a vector containing x1, x2, x3, and reservoir values 
     x1, x2, x3 = z
-    dx1dt = (-r12 - r13)*x1 + k1*(R1-x1) + r12*x2 + r13*x3
-    dx2dt = (-r12 - r23)*x2 + k2*(R2-x2) + r12*x1 + r23*x3 
-    dx3dt = r13*x1 + r23*x2 - (r13 + r23)*x3
+    v13 = r13*x1 - r13*x3*np.exp(dg13)
+    v12 = r12*x1 - r12*x2*np.exp(dg12)
+    v23 = r23*x2 - r23*x3*np.exp(dg23) 
+    dx1dt = -v12 - v13 + k1*(R1-x1)
+    dx2dt = v12 + v23 + k2*(R2-x2)
+    dx3dt = v13 - v23  
 
 
 
     return [dx1dt, dx2dt, dx3dt]
 
-tf = 5
+tf = 3
 
 sol = solve_ivp(toydt, [0, tf], z0, args=(k1, k2, r12, r13, r23), dense_output=True)
 t = np.linspace(0, tf, 300)
@@ -44,6 +63,7 @@ plt.plot(t, z.T)
 plt.xlabel('t')
 plt.ylabel('conc')
 plt.legend(['x1', 'x2', 'x3'], shadow=True)
+plt.suptitle(f"$\mu_1$ = {mu1:.2f}, $\mu_2$ = {mu2:.2f}, $\mu_3$ = {mu3:.2f}")
 plt.show()
 
 
--- rxn_sim.py
+++ rxn_sim.py
@@ -1,2 +1,41 @@
-# First case 
-# we have x1, x2, and x2
+import numpy as np
+import matplotlib.pyplot as plt 
+
+np.random.seed(42)
+
+# helper functions
+def get_G(x, N, mu0, BD):
+    mu = mu0 + np.log(x)
+    G = N.T * mu + BD
+    return G
+
+
+
+# stoichiometric matrix
+N = np.array([[1, -1, 0, -1, 0],
+              [0, -1, -1, 0, 0],
+              [0, 0, 1, 1, -1]])
+
+
+# PARAMS 
+# we choose mu for boundary
+mu_R1 = 10
+mu_R2 = 0
+
+vb1 = 1
+vb2 = 1
+# we then sample the other mu's in between these 
+mu0_1, mu0_2, mu0_3 = np.random.uniform(0, 10, 3)
+mu0 = np.array({mu0_1, mu0_2, mu0_3})
+
+
+def flux_vector(x, N, mu0, k):
+    g = get_G(x, N, mu0, BD=0)
+
+    k1, k2, k3 = k
+    v1 = vb1 * (1-np.exp(g[0]))
+    v1 = k1* x * (1-np.exp(g[1]))
+    v1 = k2 * x * (1-np.exp(g[2]))
+    v1 = k3 * x * (1-np.exp(g[3]))
+    v1 = vb2 * (1-np.exp(g[4]))
+
